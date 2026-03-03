457d8eb7e86406e304c2aa629c5d9392d7400546
Lucas Trajano Francisco <Lucas.Francisco@fgv.br>
2026-03-03 12:55:14 -0300

    Segurança na Config.py

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+import os
+
+class Settings:
+    # Informações gerais do sistema
+    PROJECT_NAME: str = "Sistema de Metas e Desempenho"
+    PROJECT_VERSION: str = "1.0.0"
+    
+    # Configuração do Banco de Dados
+    SQLALCHEMY_DATABASE_URI: str = "sqlite:///./metas.db"
+    
+    # Chave secreta para segurança (Sessões e Cookies)
+    # No futuro, se for para a nuvem, o sistema puxa a senha do servidor (os.getenv)
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "dasbhskadsja-984239$@#$23")
+
+# Cria uma variável global que poderá ser importada em outros arquivos
+settings = Settings()
